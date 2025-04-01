dd86d9466859c003b88ed973a9866ee70e576b16
justus <henze5@gmx.de>
2025-04-01 14:17:33 +0200

    Praktikum 0 Unterlagen

---
+++ Praktikum0/src/OneOhOne.java
@@ -0,0 +1,26 @@
+public class OneOhOne {
+	/**
+	 * Computes and outputs the product of two integer numbers.
+	 *
+	 * @param i first number
+	 * @param j second number
+	 */
+	public static void outputProduct(int i, int j) {
+		int product = i * j;
+
+		System.out.println(i + " * " + j + " = " + product);
+	}
+
+	/**
+	 * Main program.
+	 *
+	 * @param args arguments
+	 */
+	public static void main(String[] args) {
+		for (int i = 1; i <= 10; i++) {
+			for (int j = 1; j <= 10; j++) {
+				outputProduct(i, j);
+			}
+		}
+	}
+}
