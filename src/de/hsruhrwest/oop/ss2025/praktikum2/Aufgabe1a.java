8b84abdaee6e62d763dd3f486000aa8076929137
justin <toastbrotjustin@gmail.com>
2025-04-23 22:41:48 +0200

    Aufgabe 1a und 1b hinzugefügt

---
+++ src/de/hsruhrwest/oop/ss2025/praktikum2/Aufgabe1a.java
@@ -0,0 +1,31 @@
+package de.hsruhrwest.oop.ss2025.praktikum2;
+
+import java.util.Scanner;
+
+public class Aufgabe1a {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Geben Sie eine Zahl für n ein");
+
+        int n = scanner.nextInt();
+        if (n < 0) {
+            System.out.println("Fehler, keine negativen Zahlen");
+            scanner.close();
+            return;
+        }
+
+
+        long [] array = new long[n + 1];
+        array[0] = 1;
+        for (int i = 1; i <= n; i++ ) {
+            array[i] = array[i - 1] * i;
+        }
+
+        for (int i = 0; i <= n; i++) {
+            System.out.println("!" + i + " = " + array[i]);
+        }
+
+
+
+    }
+}
---
+++ src/de/hsruhrwest/oop/ss2025/praktikum2/Aufgabe1b.java
@@ -0,0 +1,34 @@
+package de.hsruhrwest.oop.ss2025.praktikum2;
+
+import java.util.Scanner;
+
+public class Aufgabe1b {
+    public static void main(String[] args) {
+        System.out.println("Geben sie eine Zahl für n ein");
+        Scanner scanner = new Scanner(System.in);
+
+        int n = scanner.nextInt();
+
+        if (n < 0 ) {
+            System.out.println("Fehler deine n x n matrix kann nicht negativ sein");
+            return;
+        }
+
+        int [][] matrix = new int[n][n];
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                matrix[i][j] = i+j+1;
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
+
+
