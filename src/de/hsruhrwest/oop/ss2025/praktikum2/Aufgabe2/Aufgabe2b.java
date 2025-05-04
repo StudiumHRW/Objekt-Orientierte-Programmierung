94ea03faf12b38b9e579b90b1262c3d140d7898a
justus <henze5@gmx.de>
2025-05-04 13:19:58 +0200

    löse aufgabe 2b mit Arrays

--- src/de/hsruhrwest/oop/ss2025/praktikum2/Aufgabe2/Aufgabe2b.java
+++ src/de/hsruhrwest/oop/ss2025/praktikum2/Aufgabe2/Aufgabe2b.java
@@ -1,40 +1,41 @@
 package de.hsruhrwest.oop.ss2025.praktikum2.Aufgabe2;
 
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
 import java.util.Random;
 
 public class Aufgabe2b {
 
     public static void main(String[] args) {
         // Arrays mit Zufallselementen erstellen
-        List<Integer> duplikate = new ArrayList<>();
         int upperBound = 20;
         int size = 50;
         int[] array1 = new Random().ints(0, upperBound).limit(size).toArray();
         int[] array2 = new Random().ints(0, upperBound).limit(size).toArray();
-
-        // TODO: Code gemäß Aufgabenstellung ergänzen
-
+        int[] duplikate = new int[size];
 
         System.out.println(Arrays.toString(array1));
         System.out.println(Arrays.toString(array2));
         System.out.println();
 
-        for (int i = 0; i < array1.length; i++) {
-            for (int j = 0; j < array2.length; j++) {
-                if (array1[i] == array2[j]) {
-                    if(!duplikate.contains(array1[i])) {
-                        duplikate.add(array1[i]);
-                    }
+        int amountOfDuplicates = 0;
+        for (int j : array1) {
+            if (contains(array2, j)) {
+                if (!contains(duplikate, j)) {
+                    duplikate[amountOfDuplicates++] = j;
                 }
             }
         }
+        duplikate = Arrays.copyOf(duplikate, amountOfDuplicates);
         System.out.println("Duplikate:");
-        System.out.println(duplikate);
+        System.out.println(Arrays.toString(duplikate));
+    }
+
+    private static boolean contains(int[] array, int element) {
+        for (int j : array) {
+            if (j == element) {
+                return true;
+            }
+        }
+        return false;
     }
 }
-
-
-
