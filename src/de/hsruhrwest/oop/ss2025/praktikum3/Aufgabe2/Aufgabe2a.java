ab12270686a604d5271bd43a7aaa694ffe730257
justus <henze5@gmx.de>
2025-05-14 12:34:10 +0200

    add P4 A4

--- src/de/hsruhrwest/oop/ss2025/praktikum3/Aufgabe2/Aufgabe2a.java
+++ src/de/hsruhrwest/oop/ss2025/praktikum3/Aufgabe2/Aufgabe2a.java
@@ -1,4 +1,5 @@
 package de.hsruhrwest.oop.ss2025.praktikum3.Aufgabe2;
+
 public class Aufgabe2a {
     public static long polynom(long x, long y) {
         return 9 * (x * x) - 2 * (x * x * x) + y;
@@ -8,5 +9,5 @@ public class Aufgabe2a {
     public static void main(String[] args) {
         long result = polynom(Integer.parseInt(args[0]), Integer.parseInt(args[1]));
         System.out.println(result);
-        }
     }
+}
---
+++ src/de/hsruhrwest/oop/ss2025/praktikum4/a4/Employee.java
@@ -0,0 +1,29 @@
+package de.hsruhrwest.oop.ss2025.praktikum4.a4;
+
+import java.time.LocalDate;
+
+public class Employee {
+    private final long id;
+    private final String name;
+    private final LocalDate birthDate;
+    private static long ID_COUNTER = 0;
+
+    public Employee(String name, LocalDate birthDate) {
+        this.id = ID_COUNTER++;
+        this.name = name;
+        this.birthDate = birthDate;
+    }
+
+    public long getId() {
+        Employee steve = new Employee("Steve", LocalDate.of(1970, 1, 1));
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public LocalDate getBirthDate() {
+        return birthDate;
+    }
+}
