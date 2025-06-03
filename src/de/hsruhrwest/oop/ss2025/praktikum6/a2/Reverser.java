3a5931e11c91f1f5b90d5952025de106b30596db
justus <henze5@gmx.de>
2025-06-03 10:21:27 +0200

    P6 A2 move loop to transform method

--- src/de/hsruhrwest/oop/ss2025/praktikum6/a2/Reverser.java
+++ src/de/hsruhrwest/oop/ss2025/praktikum6/a2/Reverser.java
@@ -1,15 +1,10 @@
 package de.hsruhrwest.oop.ss2025.praktikum6.a2;
 
 public class Reverser extends AbstractStringTransformer {
-    StringBuilder stringBuilder = new StringBuilder();
 
     @Override
-    protected String[] transformString(String[] array) {
-        String[] result = new String[array.length];
-        for (int i = 0; i < array.length; i++) {
-            result[i] = new StringBuilder(array[i]).reverse().toString();
-        }
-        return result;
+    protected String transformString(String string) {
+        return new StringBuilder(string).reverse().toString();
     }
 }
 
