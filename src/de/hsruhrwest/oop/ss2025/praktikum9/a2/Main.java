291b37f91bce5e2809e669f222c3ec694695db31
justus <henze5@gmx.de>
2025-06-24 10:17:57 +0200

    P9 A2 variate scores

--- src/de/hsruhrwest/oop/ss2025/praktikum9/a2/Main.java
+++ src/de/hsruhrwest/oop/ss2025/praktikum9/a2/Main.java
@@ -17,10 +17,10 @@ public class Main {
     public static void main(String[] args) {
         List<User> users = new ArrayList<>();
         users.add(new User("Tom", new User.Profile(345, LocalDate.now().minusDays(10))));
-        users.add(new User("Julian", new User.Profile(345, LocalDate.now().minusWeeks(15))));
-        users.add(new User("Angela", new User.Profile(345, LocalDate.now().minusYears(20))));
-        users.add(new User("Austin", new User.Profile(345, LocalDate.now().minusDays(1))));
-        users.add(new User("Jeffrey", new User.Profile(345, LocalDate.now().minusDays(100))));
+        users.add(new User("Julian", new User.Profile(123, LocalDate.now().minusWeeks(15))));
+        users.add(new User("Angela", new User.Profile(3124, LocalDate.now().minusYears(20))));
+        users.add(new User("Austin", new User.Profile(33, LocalDate.now().minusDays(1))));
+        users.add(new User("Jeffrey", new User.Profile(11, LocalDate.now().minusDays(100))));
 
         printMatchingUsers(users, user -> user.getProfile().getScore() > 300);
         printMatchingUsers(users, user -> user.getProfile().getMemberSince().isAfter(LocalDate.now().minusDays(100)));
